65c7dcfebe9a6026a0257e74dff0381f13f208d2
Caleb <origami.tipster.0q@icloud.com>
2023-12-12 12:47:49 +0200

    Updated

--- README.md
+++ README.md
@@ -141,8 +141,10 @@ While there are existing tools that provide Bitcoin price information, our appli
 1. Clone the repository:
 
    ```bash
-   git clone https://github.com/your-username/todo-list-app.git
-   cd todo-list-app
+   git clone https://github.com/summa-ingenio/bitcoin-arbitrage
+   cd pricing server
+   cd transactions-client
+   cd transactions-sever
 
    ```
 
@@ -189,3 +191,5 @@ Username: caleb@gmail.com
 Password: test
 
 ## Deployment URL
+
+https://finaltask-client-139e56f14968.herokuapp.com/
--- src/components/users.js
+++ src/components/users.js
@@ -30,6 +30,25 @@ const UserList = () => {
     fetchUsers();
   }, [token]); // Include token as a dependency to re-fetch when it changes
 
+  // Function to handle user deletion
+  const handleDeleteUser = async (userId) => {
+    try {
+      await axios.delete(
+        `https://finaltask-server-950d32b6c3a7.herokuapp.com/api/users/${userId}`,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+
+      // Update the users list after deletion
+      setUsers((prevUsers) => prevUsers.filter((user) => user._id !== userId));
+    } catch (error) {
+      console.error("Failed to delete user", error);
+    }
+  };
+
   return (
     <div>
       <Navbar />
@@ -39,12 +58,18 @@ const UserList = () => {
           <thead>
             <tr>
               <th>Email</th>
+              <th>Action</th> {/* Add a new column for the delete button */}
             </tr>
           </thead>
           <tbody>
             {users.map((user) => (
-              <tr key={user.username}>
+              <tr key={user._id}>
                 <td>{user.username}</td>
+                <td>
+                  <button onClick={() => handleDeleteUser(user._id)}>
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
